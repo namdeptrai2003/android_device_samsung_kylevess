d1aaf1e8ca3422f3c1889239c27b43e16c1a2cd7
fbli41 <berkantk3@gmail.com>
2016-02-28 16:27:30 +0200

    device_kylevess.mk: Don't use common tree

--- device_kylevess.mk
+++ device_kylevess.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/kylevexx/kylevexx-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/kylevess/kylevess-common-vendor.mk)
 
 # Use high-density artwork where available
 PRODUCT_LOCALES += hdpi
